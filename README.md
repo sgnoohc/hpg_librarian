3e213f5a80bd265df3460d4befe48a87e75854f8
Philip Chang <philip@ucsd.edu>
2023-09-11 10:57:28 -0400

    update

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
     srun --partition=gpu --cpus=8 --gpus=1 --mem=16gb --constraint=a100 --pty bash -i
     srun --partition=gpu          --gpus=1 --mem=16gb --constraint=a100 --pty bash -i
-# hpg_librarian
+
+    sacct -n -a -S $(date +%F) -q avery --format=User%20,JobID%30,JobName%30,Partition%30,State,Submit,Start,End,ElapsedRaw,AllocCPUs > data.txt
+
+Printing out # of currently running avery CPUs:
+
+    squeue --qos=avery -o "%.12C" -h -t R | awk '{total += $1} END {print total}'
---
+++ parse.py
@@ -0,0 +1,183 @@
+#!/bin/env python3
+
+import csv
+import time
+import sys
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+import math
+import pytz
+
+#----------------------------------------
+# Configuration
+time_res = 60 # 10 minutes
+time_window = 432000 # 5 days
+#----------------------------------------
+
+# Current time
+eastern_timezone = pytz.timezone('US/Eastern')
+current_time = datetime.datetime.now(eastern_timezone)
+nearest_10_minutes = math.ceil(current_time.minute / 10) * 10 # Calculate the nearest 10 minutes
+if nearest_10_minutes == 60:
+    nearest_10_minutes = 59
+rounded_time = current_time.replace(minute=nearest_10_minutes, second=0, microsecond=0) # Round the time to the nearest 10 minutes
+current_time = int(rounded_time.timestamp()) # Convert the rounded time to Unix timestamp
+
+# Compute time bin boundaries
+to_time = current_time
+from_time = current_time - time_window
+from_date = datetime.datetime.fromtimestamp(from_time).strftime('%Y-%m-%d')
+nbins = int(time_window / time_res)
+
+# Goal is to create histograms of n bins where each bin contains number of CPUs / memory per user
+time_idxs = [ from_time + time_res * x for x in range(nbins) ]
+d = {}
+observables = [
+        "NCPUS",
+        "NGPUS",
+        "ReqMem"
+        ]
+thresholds = {
+        "NCPUS" : 430,
+        "NGPUS" : 14,
+        "ReqMem" : 3359,
+        }
+
+# Following are the data to obtain
+column_headers = [
+        "User",
+        "Submit",
+        "NNodes",
+        "NCPUS",
+        "ReqMem",
+        "ReqTRES",
+        "State",
+        "Priority",
+        "Start",
+        "ElapsedRaw",
+        "JobID",
+        "JobIDRaw",
+        "NodeList",
+        "Reason",
+        ]
+
+# Creating some colunm name to integer matching
+cols = {}
+for i, c in enumerate(column_headers):
+    cols[c] = i
+
+# Process the Slurm command and obtain the data
+columns = ",".join(column_headers)
+os.system(f"sacct -a -S {from_date} -q avery --format=\"{columns}\" -P --noconvert > data.txt")
+
+df = pd.read_csv("data.txt", sep="|")
+df = df[~df.User.isna()] # Get rid of rows with user name NaN
+
+df["Start"] = df["Start"].apply(lambda x : int(time.mktime(time.strptime(x, "%Y-%m-%dT%H:%M:%S"))) if x != "Unknown" else 0)
+df["Submit"] = df["Submit"].apply(lambda x : int(time.mktime(time.strptime(x, "%Y-%m-%dT%H:%M:%S"))) if x != "Unknown" else 0)
+df["ElapsedRaw"] = df["ElapsedRaw"].apply(lambda x : int(x))
+df["End"] = df["Start"] + df["ElapsedRaw"]
+df['NGPUS'] = df["ReqTRES"].apply(lambda x : int(x.split("gpu=")[1].split(",")[0]) if "gpu" in x else 0)
+df['ReqMem'] = df["ReqMem"].apply(lambda x : int(x.replace("M", "")) / 1024)
+def mystate(x):
+    if "PENDING" in x: return 0
+    if "RUNNING" in x: return 1
+    if "COMPLETED" in x: return 2
+    if "TIMEOUT" in x: return 3
+    if "CANCELLED" in x: return 4
+    if "FAILED" in x: return 5
+df['State'] = df['State'].apply(mystate)
+
+# Following are the data to obtain
+selected_columns = [
+        "User",
+        "Submit",
+        "NCPUS",
+        "NGPUS",
+        "ReqMem",
+        "State",
+        "Priority",
+        "Start",
+        "End",
+        "JobIDRaw",
+        "NodeList",
+        "Reason",
+        ]
+
+# Subselect the columns
+df = df[selected_columns]
+
+# Get all users within this time period
+users = df["User"].value_counts().index.tolist()
+
+# Loop over time bin boundaries
+for i in range(nbins):
+
+    # If first bin create list
+    if i == 0:
+        for user in users:
+            d[user] = {}
+            for observable in observables:
+                d[user][observable] = [0] * nbins
+
+    # Obtain time bin boundary
+    bt = from_time + time_res * (i)
+    et = from_time + time_res * (i + 1)
+
+    # Subselect data within time bin boundary
+    df_timebin = df.loc[(df.Start < et) & (df.End > bt)]
+
+    # Compute each length of job in the given time bin (some jobs may have started in this bin, some may have ended in this bin)
+    df_timebin["WithinTimeBinEnd"] = df_timebin["End"].apply(lambda x : x if x < et else et)
+    df_timebin["WithinTimeBinStart"] = df_timebin["Start"].apply(lambda x : x if x > bt else bt)
+    df_timebin["WithinTimeBinElapsed"] = df_timebin["WithinTimeBinEnd"] - df_timebin["WithinTimeBinStart"]
+
+    # Obtain active users with jobs within time bin boundary
+    active_users = df_timebin["User"].value_counts().index.tolist()
+
+    # Get usages for each user
+    for active_user in active_users:
+        df_timebin_user = df_timebin[df_timebin["User"] == active_user]
+        for observable in observables:
+            res = df_timebin_user[observable] * df_timebin_user["WithinTimeBinElapsed"] / time_res
+            user_sum = res.sum()
+            d[active_user][observable][i] = user_sum
+
+dfs = []
+for observable in observables:
+    data = {}
+    for user in users:
+        data[user] = d[user][observable]
+
+    df = pd.DataFrame(data, index = time_idxs)
+    df.index = pd.to_datetime(df.index, unit='s')
+    df['Total'] = df[users].sum(axis=1)
+    max_value = df[['Total'] + users].max().max()
+    threshold_value = thresholds[observable]
+
+    # Create the stacked area plot
+    plt.figure(figsize=(10, 6))
+    stack_data = [df[col] for col in users]
+    plt.stackplot(df.index, stack_data, labels=users)
+    plt.plot(df.index, df['Total'], color='black', linewidth=2, label='Total')
+    plt.title(f'{observable} Usage Over Time (MAX: {threshold_value})')
+    plt.xlabel('Time')
+    plt.ylabel(f'{observable} Usage')
+    plt.grid(True)
+    plt.legend(loc='upper left')
+
+    # Rotate x-axis labels for readability (optional)
+    plt.xticks(rotation=45)
+
+    # Set the max value
+    plt.ylim(0, 1.7 * max_value)
+
+    # Add a threshold line of maximum usage
+    plt.axhline(y=threshold_value, color='red', linestyle='--', label=f'Threshold ({threshold_value})')
+
+    # Show or save the plot
+    plt.tight_layout()
+    plt.savefig(f"{observable}.png")
+    plt.savefig(f"{observable}.pdf")
